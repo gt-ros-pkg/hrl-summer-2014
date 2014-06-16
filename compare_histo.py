ca070ef5d7b3b1e46df85018f265d993658df575
CyphFalcon <crlittl@g.clemson.edu>
2014-06-16 11:35:35 -0400

    Update compare_histo.py
    Updated to, upon successful checks, update the stored image.

--- compare_histo.py
+++ compare_histo.py
@@ -6,8 +6,10 @@
 # It locates two files, expected.jpeg and snapshot.jpeg, in the directory
 # CoHis, and preforms a deviation check of their histograms. If the difference
 # is greater than the expected value (50), the server returns 0, whereas if 
-# the difference is acceptable, the server returns 1. If only one file was located,
-# the server returns a -1.
+# the difference is acceptable, the server returns 1. Given a succesful 
+# comparison, the server will update the stored image. If only one file was 
+# located, the server returns a -1. Must be run in the same folder as 
+# snap_node.py! (Taken care of by launch file.)
 import roslib; roslib.load_manifest('snapshot')
 from snapshot.srv import *
 import rospy
@@ -17,18 +19,26 @@ from matplotlib import pyplot as plt
 
 
 def analyze_image(num):
-    print "I've been Called"
+    print "I've been called - analyze_image"
     img = cv2.imread('./CoHis/expected.jpeg',0)
     hist_norm = cv2.calcHist([img], [0], None, [256], [0,256])
     current = cv2.imread('./CoHis/snapshot.jpeg',0)
     hist_current = cv2.calcHist([current],[0],None,[256],[0,256])
+    
+    #Uncomment below to see what images are being processed
+    #cv2.imshow('compared to', img)
+    #cv2.imshow('comparing', current)
+    #cv2.waitKey(4500)
+    #cv2.destroyAllWindows()
+
     sum_norm = 0
     sum_current = 0
     for i in hist_current:
 	if i != 0:
 	   sum_current = 1
     if sum_current == 0:
-	return -1
+	print "Unable to locate snapshot.jpeg. This error is expected duing the first run."
+        return -1
     else:
 	sum_current = 0
 
@@ -38,13 +48,15 @@ def analyze_image(num):
            sum_current = sum_current + hist_current[i]
     print "#%d#"%abs(sum_norm - sum_current)
     if abs(sum_norm - sum_current) > 50:
-     return 0
+     return abs(sum_norm - sum_current)
     else:
+     cv2.imwrite('./CoHis/expected.jpeg', current)
      return 1
+
 def compare_histo():
-    print "Running"
+    print "I'm running - compare_histo"
     rospy.init_node('compare_histo')
-    hisp = rospy.Service('compare_histo',CompareHisto, analyze_image)
+    hisp = rospy.Service('compare_histo', CompareHisto, analyze_image)
     rospy.spin()
 
 if __name__ == '__main__':
