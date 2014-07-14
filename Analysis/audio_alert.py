bff4be3028a3b394dfe9eed5ff90c64b1bac9957
mrich7 <mrich7@gatech.edu>
2014-07-14 15:04:24 -0400

    Update audio_alert.py

--- Analysis/audio_alert.py
+++ Analysis/audio_alert.py
@@ -35,34 +35,37 @@ secs=5          		#number of seconds to establish background noise baseline
 stop=True 
 mu=0			        #mean as calculated from previous trials
 				#variance
-sigma=120.96
+sigma=140
 				#standard deviation	
 stddevs=1	          	#number of standard deviations above mean to allow as a threshhold
 print(str(sigma*stddevs))
-
+rospy.init_node('audio_talker', anonymous=False)
 pub1=rospy.Publisher('emergency', String)
 pub2=rospy.Publisher('audio_analysis', numpy_msg(Floats))
+
 
 p=pyaudio.PyAudio()
 
 stream=p.open(format=form, channels=channel, rate=rate1, input=True, frames_per_buffer=chunk)
 
-print ("*recording background noise")
 
-frames_back=deque([])
-amp_back=deque([])
-for i in range(0, int(rate1/chunk*secs)):     #Record a couple (5) seconds of background noise
-    data=stream.read(chunk)
-    decoded=np.fromstring(data, 'Float32')
-    decoded2=np.fromstring(data, 'Int16')
-    frames_back.append(decoded)
-    amp_back=deque([])
+#print ("*recording background noise")
 
-print("*done recording background noise")
+#frames_back=deque([])
+#amp_back=deque([])
+#for i in range(0, int(rate1/chunk*secs)):     #Record a couple (5) seconds of background noise
+#    data=stream.read(chunk)
+#    decoded=np.fromstring(data, 'Float32')
+#    decoded2=np.fromstring(data, 'Int16')
+#    frames_back.append(decoded)
+#    amp_back=deque([])
+
+#print("*done recording background noise")
 
 
 #Model of Background Noise
-mu_back=np.mean(amp_back)
+
+#mu_back=np.mean(amp_back)
 
 
 
@@ -78,13 +81,20 @@ try:
         frames.append(decoded)
         l=len(frames)
         if l*chunk>=3000:
-
-            frames_arr=np.array(frames)    
+            index=range(0, l-2)
+            frames_arr=np.array(frames.popleft())   
+            amp_frames_arr=np.array(amp_frames.popleft())
+            for i in index:
+                e=frames.popleft()
+                frames_arr=np.concatenate((frames_arr, e), axis=0)
+            for i in index:
+                s=amp_frames.popleft()
+                amp_frames_arr=np.concatenate((amp_frames_arr, s), axis=0)
             frames_arr.reshape(-1)    
 
 
             #DFT Spectrogram
-            Pxx, freqs, t=mlab.specgram(frames_arr[:], NFFT=256, Fs=rate1)
+            Pxx, freqs, t=mlab.specgram(frames_arr, NFFT=256, Fs=rate1)
             
             #Find average power       
             Pxx_ave=np.mean(Pxx, axis=1)
@@ -95,40 +105,75 @@ try:
             #Amalgamate into one 1D array, so it can be sent as a ROS message
             data=np.concatenate((c, d)) 
             #Make histogram of frequency vs power spectral density
-            H, xedges, yedges=np.histogram2d(Pxx_ave, freqs, bins=16, normed=True)
+            #H, xedges, yedges=np.histogram2d(Pxx_ave, freqs, bins=16, normed=True)
                    
             #Find Z score with which to check amplitude
-            z=(np.array(amp_frames, dtype='int16')-(mu+mu_back.astype('int16')))/sigma
-            
-           
+            z=((amp_frames_arr-mu)/sigma)
+            print(z)
+            a=abs(z)>=stddevs*sigma
+            print(a.any())           
             #Publish ROS messages
     	    
-            def audio_analyzer(stop, z, stddevs, data): 
+            def audio_analyzer(z, data): 
                 global pub1
                 global pub2
-                rospy.init_node('audio_talker', anonymous=False)
+                
                 r=rospy.Rate(10) #in hz
                 #Check if amplitude is above threshold
-                a=z>=stddevs*sigma
-                print(a.any())
-                try:
-                    while not rospy.is_shutdown():
-                    	a=z>=stddevs*sigma
-                    	if a.any():
-                        	#Continue checking amplitude and publish stop message if anomaly occurs
-                        	str='STOP'
-                        	pub1.publish(str)
-                    	else:
-                        	#If no anomaly continue publishing power and frequency data
-                        	pub2.publish(data.astype(float))
-                        	rospy.sleep(0.1)
-                except KeyboardInterrupt:
-                    rospy.shutdown()
+                a=abs(z)>=stddevs*sigma
+                b=a.any()
+                
+                if b:
+                    #Continue checking amplitude and publish stop message if anomaly occurs
+                    pub1.publish('STOP')
+                    pub1.publish('STOP')
+                    pub1.publish('STOP')
+                    pub1.publish('STOP')
+                    pub1.publish('STOP')
+                    pub1.publish('STOP')
+                    pub1.publish('STOP')
+                    pub1.publish('STOP')
+                    pub1.publish('STOP')
+                    pub1.publish('STOP')
+                    rospy.sleep(0.1)
+                    print('help')
+                    rospy.sleep(0.1)                
+                else:
+                    
+                    #If no anomaly continue publishing power and frequency data
+                    pub2.publish(data.astype(float))
+                
+                while not rospy.is_shutdown():
+                    a=abs(z)>=stddevs*sigma
+                    b=a.any()
+                    
+                    if b:
+                        #Continue checking amplitude and publish stop message if anomaly occurs
+                        str1='STOP'
+                        pub1.publish(str1)
+                        pub1.publish('STOP')
+                        pub1.publish('STOP')
+                        pub1.publish('STOP')
+                        pub1.publish('STOP')
+                        pub1.publish('STOP')
+                        pub1.publish('STOP')
+                        pub1.publish('STOP')
+                        pub1.publish('STOP')
+                        pub1.publish('STOP')
+                        pub1.publish('STOP')
+                        rospy.sleep(0.1)
+                        print('work')
+                        rospy.sleep(0.1)  
+                    else:
+                        
+                        #If no anomaly continue publishing power and frequency data
+                        pub2.publish(data.astype(float))
+                    
+              
 
             if __name__=='__main__':
-                try:
-                    audio_analyzer(stop, z, stddevs, data)
-                except rospy.ROSInterruptException: pass
+                audio_analyzer(z, data)
+                
                  
 
 except KeyboardInterrupt: 
