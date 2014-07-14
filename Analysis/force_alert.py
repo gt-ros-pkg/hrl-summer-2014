5ed641d3ca382ec3786f9d955de9fbce3c4fdc36
mrich7 <mrich7@gatech.edu>
2014-07-14 15:31:43 -0400

    Update force_alert.py

--- Analysis/force_alert.py
+++ Analysis/force_alert.py
@@ -1,11 +1,5 @@
+
 #! /usr/bin/env python
-#
-#     This node will collect the force messages from the force-torque sensor at the gripper the PR2,
-#     calculate the magnitude of the force, and--using a statistical model based on previous trials
-#     of the PR2 performing this task--publish an alert message to the 'emergency' topic if the magnitude
-#     of force is two standard deviations above the mean. Otherwise, it will publish the magnitude
-#     of the force.
-#
 
 from __future__ import division
 import numpy as np
@@ -21,7 +15,7 @@ import math
 
 hand='l'                   #which gripper is being tracked
 mu=16.97                   #mean magnitude of force as calculated from previous trials in Newtons
-sigma2=0.1
+sigma2=0.9
 sigma=math.sqrt(sigma2)
 dist= stats.norm(mu,sigma) #unit gaussian curve of amplitude of previous tests
 stddev=2	           #number of standard deviations above mean to allow as threshhold
@@ -32,22 +26,34 @@ stddev=2	           #number of standard deviations above mean to allow as thresh
 class force_analysis ():
     def __init__(self):
         rospy.init_node('FT_listener', anonymous=False)
-        rospy.Subscriber("ft/%s_gripper_motor"%hand, WrenchStamped, self.callback)
-        self.pub=rospy.Publisher('emergency', String)
-        self.pub2=rospy.Publisher('Force_magnitude', String)
-        self.r=rospy.Rate(10) #in hz
         self.fmag=deque([])
         self.force_x=deque([])
         self.force_y=deque([])
         self.force_z=deque([])
+        self.ignore=False
+        self.pub=rospy.Publisher('emergency', String)
+        self.pub2=rospy.Publisher('Force_magnitude', String)
+        rospy.Subscriber("ft/%s_gripper_motor"%hand, WrenchStamped, self.callback)
+        rospy.Subscriber('Main_Control', String, self.listen)
+        
+        self.r=rospy.Rate(10) #in hz
 
     def callback(self, msg):
-        #grab force messages from force-torque sensor
         self.fx=msg.wrench.force.x
         self.fy=msg.wrench.force.y
         self.fz=msg.wrench.force.z
         self.calculate()
 
+    def listen(self, msg):
+        self.message=msg.data
+        if "SwitchControllers" in self.message:
+            self.ignore=True
+        elif "FacePos1" in self.message: 
+            self.ignore=False
+        else:
+            self.ignore=False
+       
+        
     def calculate (self):
         self.force_x.append(self.fx)
         self.force_y.append(self.fy)
@@ -59,10 +65,9 @@ class force_analysis ():
                
         #Check magnitude   
         z=(np.array(self.fmag)-mu)/sigma
-        
-        #If anomaly detected publish stop message, otherwise publish magnitude            
+       
         a=z>=stddev*sigma
-        if a.any():    
+        if a.any() and not self.ignore:    
             str1='STOP'                             
             self.pub.publish(str1)
         else:
