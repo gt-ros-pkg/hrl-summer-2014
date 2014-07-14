97feb8f59c36115420c3f35b0e9c79f070bc2dbd
cmbirmingham <cbirmingham@zagmail.gonzaga.edu>
2014-07-14 15:34:04 -0400

    Update compare_histo.py
    version for live demo

--- Histogram/compare_histo.py
+++ Histogram/compare_histo.py
@@ -18,40 +18,38 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 
-check = 0
-
 def analyze_image(num):
-    global check
     print "I've been called - analyze_image"
     img = cv2.imread('./CoHis/expected.jpeg')
-    hist_norm = cv2.calcHist([img], [0], None, [256], [0,256])
+    mask = np.zeros(img.shape[:2], np.uint8)
+    mask[375:585,540:725] = 225
+    hist_norm = cv2.calcHist([img], [0], mask, [256], [0,256])
     current = cv2.imread('./CoHis/snapshot.jpeg')
-    hist_current = cv2.calcHist([current],[0],None,[256],[0,256])
-    
+    hist_current = cv2.calcHist([current],[0],mask,[256],[0,256])
+    current_masked = cv2.bitwise_and(current,current, mask = mask)
     sum_norm = 0
     sum_current = 0
     for i in hist_current:
 	if i != 0:
 	   sum_current = 1
-    if sum_current == 0 or check == 1:
-	print "Unable to locate snapshot.jpeg. This error is expected duing the first run."
-        check = 0
+    if sum_current == 0:
+	print "Unable to locate snapshot.jpeg. This error is expected during the first run."
         return -1
     else:
 	sum_current = 0
     
     #Uncomment below to see what images are being processed
     #cv2.imshow('compared to', img)
-    #cv2.imshow('comparing', current)
+    #cv2.imshow('comparing', current_masked)
     #cv2.waitKey(4500)
     #cv2.destroyAllWindows()
 
-    for i in range(0,len(hist_norm)): # will need to update this with appropriate area
+    for i in range(0,len(hist_norm)): # will need to update this with appropriate area mask
         if i > 1 and i < 255: 
 	   sum_norm = sum_norm + hist_norm[i]
            sum_current = sum_current + hist_current[i]
     print "#%d#"%abs(sum_norm - sum_current)
-    if abs(sum_norm - sum_current) > 50:
+    if abs(sum_norm - sum_current) > 100:
      return 0
     else:
      os.rename('./CoHis/snapshot.jpeg','./CoHis/expected.jpeg')
@@ -60,8 +58,6 @@ def analyze_image(num):
 def compare_histo():
     print "I'm running - compare_histo"
     rospy.init_node('compare_histo')
-    global check
-    check = 1
     hisp = rospy.Service('compare_histo', CompareHisto, analyze_image)
     rospy.spin()
 
