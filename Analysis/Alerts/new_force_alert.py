e1a21c060092f9ecc1704cdc9279dff9b9c6891b
mrich7 <mrich7@gatech.edu>
2014-07-24 16:32:59 -0400

    Update new_force_alert.py

--- Analysis/Alerts/new_force_alert.py
+++ Analysis/Alerts/new_force_alert.py
@@ -20,7 +20,7 @@ import math
 
 hand='l'                   #which gripper is being tracked
 mus=np.array([11.1, 12.1, 15.78, 16.41, 16.29, 17.07, 16.83, 12.04, 9.52, 11.46, 12.67, 14.99]) #mean magnitude of force as calculated from previous trials in Newtons
-sigma2=np.array([1.4, 3.59, 1.0, 0.11, 0.81, 1.49, 2.01, 4.82, 0.80, 1.27, 0.09, 5.18])                 #variance
+sigma2=np.array([1.4, 3.59, 1.0, 0.11, 0.81, 1.49, 2.01, 4.82, 0.80, 1.27, 0.09, 5.18])                 #variances
 sigmas=sigma2**0.5    #standard deviation
 dist= stats.norm(mus[0],sigma) #unit gaussian distribution of magnitude of previous tests
 stddev=1	               #number of standard deviations above mean to allow as threshhold
@@ -47,7 +47,7 @@ class force_analysis ():
         self.fz=msg.wrench.force.z
         self.calculate()
 
-    def listen(self, msg):           
+    def listen(self, msg):           #listen to which part of the task is being performed
         self.message=msg.data        
         if self.message[-2] is 't':
             self.part=self.message[-1]
@@ -57,8 +57,8 @@ class force_analysis ():
             elif self.message[-1] is '1':
                 self.part='11' 					
         
-        self.mu=mus[int(self.part)]
-        self.sigma=sigmas[int(self.part)]
+        self.mu=mus[int(self.part)]        #change mean and standard deviation to those of the model for
+        self.sigma=sigmas[int(self.part)]  #the subtask being performed
         self.calculate
         
     def calculate (self):
@@ -70,10 +70,10 @@ class force_analysis ():
         self.fmag.append(math.sqrt(self.force_x[-1]**2+self.force_y[-1]**2+self.force_z[-1]**2))
         
                
-        #Check magnitude   
+        #Calculate the z-score for the magnitude of the force  
         z=(np.array(self.fmag[-1])-self.mu)/self.sigma
-        score=abs(z)-(stddev*self.sigma)
-        self.pub1.publish(score) #pusblish z-score of magnitude of force
+        score=abs(z)-(stddev*self.sigma) #check the difference between z-score and set number of standard deviations
+        self.pub1.publish(score) #pusblish this difference
                
      
 
