c14c196b55462ff91926dc78d0bbd97e491afe58
CyphFalcon <CyphFalcon@users.noreply.github.com>
2014-07-25 09:00:08 -0400

    Update anomaly_main.py
    Adjusted values, increased the number of stop messages sent, changed update system to flow better 
    (loss of a little bit of accuracy in exchange for faster updates)

--- Analysis/anomaly_main.py
+++ Analysis/anomaly_main.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+# File: anomaly_main.py
+# Caleb Little, GATech HRL summer project 2014, 7-25-14
+# This file is used as an overhead coordinator for the 
+# Guassian RV nodes of acceleration, force, and torque
+# for TEAM RYDS robot feeding robot.
+
+# Please use the following estimated target thresholds:
+# Acceleration: 1.1
+# Force: 0.4
+# Torque: 2.5
+# Master: 265
+
 import rospy
 import numpy as np
 from std_msgs.msg import Float64, String
@@ -8,63 +20,48 @@ class Investigator:
     def react(self):
         self.response = 0
         self.force_response = 0
-        self.force_check = True
         self.torque_response = 0
-        self.torque_check = True
         self.accel_response = 0
-        self.accel_check = True
-        self.sensitivity = 50 # Adjustable sensitivity. 0% -> 100%
+        self.sensitivity = 265 # Adjustable sensitivity. 0% -> 300% (adjusted at 265 based on test data)
         print "Anomaly Detection Node: Online"
-        rospy.Subscriber('Force_Result', Float64, self.Force)
-        rospy.Subscriber('Accel_Result', Float64, self.Accel)
-        rospy.Subscriber('Torque_Result', Float64, self.Torque)
+        rospy.Subscriber('Force_Results', Float64, self.Force)
+        rospy.Subscriber('Accel_Results', Float64, self.Accel)
+        rospy.Subscriber('Torque_Results', Float64, self.Torque)
 	overwatch = rospy.Publisher('emergency', String)
         rospy.init_node('Anomaly_Control')
         k = 0
 
-        while (not rospy.is_shutdown()) :
+        while (not rospy.is_shutdown()):
             satisfaction = 0
-            if self.response > 2:
-                self.response = self.response - 3
-                satisfaction = 0
-                print "Making Decision"
-                if (self.torque_response + self.force_response + self.accel_response)*100 > self.sensitivity:
-                    satisfaction = 1
-
-                self.torque_check = True
-                self.force_check = True
-                self.accel_check = True
-                if satisfaction == 1:
-                    print "Anomaly Possible!"
-                    k = k + 1
-                    if k > 10: # Require 10 hits in a row to trigger alarm
-                        print "Anomaly Detected!"
-                        overwatch.publish("STOP")
-                    else:
-                        k = 0
+            if (self.torque_response + self.force_response + self.accel_response)*100 > self.sensitivity:
+                satisfaction = 1
+            if satisfaction == 1:
+                print "Anomaly Possible!"
+                k = k + 1
+                if k > 20: # Require 20 hits in a row to trigger alarm
+                    print "Anomaly Detected!"
+                    overwatch.publish("STOP")
+                    overwatch.publish("STOP")
+                    overwatch.publish("STOP")
+                    overwatch.publish("STOP")
+                    overwatch.publish("STOP")
                 else:
-                    print "All good!"
+                    k = 0
+            else:
+                print "All good!"
+            rospy.sleep(0.01) # added buffer delay (may need to adjust)
     
     def Force(self,data):
-        if self.force_check == True:
-            print "Force result Recieved"
-            self.response = self.response + 1
-            self.force_response = data.data
-            self.force_check = False
+        print "Force result Recieved"
+        self.force_response = data.data
 
     def Accel(self,data):
-        if self.accel_check == True:
-            print "Accelerometer result Recieved"
-            self.response = self.response + 1
-            self.accel_response = data.data
-            self.force_check = False
+        print "Accelerometer result Recieved"
+        self.accel_response = data.data
     
     def Torque(self,data):
-        if self.torque_check == True:
-            print "Torque result Recieved"
-            self.response = self.response + 1
-            self.torque_response = data.data
-            self.torque_check = False
+        print "Torque result Recieved"
+        self.torque_response = data.data
 
 if __name__ == "__main__":
     ex = Investigator()
